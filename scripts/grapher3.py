d9f3c47c41a9708a0861b6441d3a5bb6123f6a14
jay <jonashamill@me.com>
2023-08-05 17:28:49 +0100

    latest results

--- scripts/grapher3.py
+++ scripts/grapher3.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def main():
+def main(file, type):
     # Read the data from the CSV file
     file = 'arlog_20230801202825'
     data = pd.read_csv('../logs/'+ file +'.csv')
@@ -13,26 +13,31 @@ def main():
     max_velocity = data['Max Velocity']
     ids = data['ID']
 
+    # Convert velocities to percentage of max value
+    max_value = max(max_velocity)
+    min_velocity_percent = [(v / max_value) * 100 for v in min_velocity]
+    max_velocity_percent = [(v / max_value) * 100 for v in max_velocity]
+
     # Create the figure and axes objects
     fig, ax1 = plt.subplots()
 
     # Create the line graph for max and min velocities on the left y-axis
-    ax1.plot(time, min_velocity, label='Min Velocity', color='blue', marker='o')
-    ax1.plot(time, max_velocity, label='Max Velocity', color='red', marker='x')
+    ax1.plot(time, min_velocity_percent, label='Min Velocity', color='blue', marker='o')
+    ax1.plot(time, max_velocity_percent, label='Max Velocity', color='red', marker='x')
 
     # Add labeled dots/crosses for each ID on the left y-axis
     for i, id_val in enumerate(ids):
         if i == 0:  # Display ID label only once
-            ax1.text(time[i], min_velocity[i], 'ID: {}'.format(id_val), ha='right', va='bottom', fontsize=8)
-            ax1.text(time[i], max_velocity[i], 'ID: {}'.format(id_val), ha='right', va='bottom', fontsize=8)
+            ax1.text(time[i], min_velocity_percent[i], 'ID: {}'.format(id_val), ha='right', va='bottom', fontsize=8)
+            ax1.text(time[i], max_velocity_percent[i], 'ID: {}'.format(id_val), ha='right', va='bottom', fontsize=8)
         else:
-            ax1.scatter(time[i], min_velocity[i], color='blue', marker='o')
-            ax1.scatter(time[i], max_velocity[i], color='red', marker='x')
+            ax1.scatter(time[i], min_velocity_percent[i], color='blue', marker='o')
+            ax1.scatter(time[i], max_velocity_percent[i], color='red', marker='x')
 
     # Set axis labels and title for left y-axis
     ax1.set_xlabel('Time (s)')
-    ax1.set_ylabel('Velocity (m/s)')
-    ax1.set_title('With Plastic Behaviour')
+    ax1.set_ylabel('Velocity (% of Max)')
+    ax1.set_title('With '+ type +' Behaviour')
 
     # Add legend for left y-axis
     ax1.legend(loc='upper left')
@@ -50,18 +55,19 @@ def main():
 
     # Set labels for the left side of the y-axis
     ax1.yaxis.set_label_position("left")
-    ax1.set_ylabel("Velocity (m/s)", fontsize=10)
+    ax1.set_ylabel("Velocity (% of Max)", fontsize=10)
 
     # Show the plot
     plt.grid(True)
     plt.tight_layout()
 
     # Save the plot as a PNG file
-    plt.savefig('../graphs/'+ file +'2.png', dpi=300)  # dpi parameter controls the resolution of the image
+    plt.savefig('../graphs/'+ file +'3.png', dpi=300)  # dpi parameter controls the resolution of the image
 
     # Show the plot
     plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    main('arlog_20230801202825', 'Plastic')
+    main('arlog_20230801201926', 'Static')
