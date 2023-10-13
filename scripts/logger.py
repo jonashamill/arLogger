123fecde7aecc1751453b0600b55cd548e3eacf7
Jay <Jonas.Hamill@bristol.ac.uk>
2023-10-13 16:19:54 +0100

    fixing where lock wasn't included

--- scripts/logger.py
+++ scripts/logger.py
@@ -261,7 +261,7 @@ def checkTimeHigh():
         timeVar += 1
 
         if timeVar >= timeThresholdHigh:
-            with activityLevel:
+            with activityLevelLock:
                 activityLevel -= 5
 
                 activityLevel = max(0, min(100, activityLevel))
